b60d43fe50a652fcdbfb7d1ef5540e36a7ad4308
Marcelo Peñas <marcelopenas@outlook.com>
2024-09-05 12:46:27 +0000

    adders

--- ula_modules.py
+++ ula_modules.py
@@ -19,23 +19,14 @@ def halfAdder(a, b, soma, carry):
 
 @block
 def fullAdder(a, b, c, soma, carry):
-    # adder_1_soma = Signal(bool(0))
-    # adder_1_carry = Signal(bool(0))
-    # adder_2_soma = Signal(bool(0))
-    # adder_2_carry = Signal(bool(0))
-
-    # adder_1 = halfAdder(a, b, adder_1_soma, adder_1_carry)
-    # adder_2 = halfAdder(c, adder_1_soma, adder_2_soma, adder_2_carry)
-
     s = [Signal(bool(0)) for _ in range(3)]
+    haList = [None for _ in range(2)]
 
-    adder_1 = halfAdder(a, b, s[0], s[1])
-    adder_2 = halfAdder(c, s[0], soma, s[2])
+    haList[0] = halfAdder(a, b, s[0], s[1])
+    haList[1] = halfAdder(c, s[0], soma, s[2])
 
     @always_comb
     def comb():
-        # soma.next = adder_2_soma
-        # carry.next = adder_1_carry or adder_2_carry
         carry.next = s[1] or s[2]
 
     return instances()
@@ -43,13 +34,33 @@ def fullAdder(a, b, c, soma, carry):
 
 @block
 def adder2bits(x, y, soma, carry):
+    s_soma = [Signal(bool(0)) for _ in range(2)]
+    half_adder_1_carry = Signal(bool(0))
+    full_adder_2_carry = Signal(bool(0))
+
+    half_adder_1 = halfAdder(x[0], y[0], soma[0], half_adder_1_carry)
+    full_adder_2 = fullAdder(x[1], y[1], half_adder_1_carry, soma[1], full_adder_2_carry)
+
+
+    @always_comb
+    def comb():
+        carry.next = full_adder_2_carry
+
     return instances()
 
 
 @block
 def adder(x, y, soma, carry):
+    size = len(x)
+    faList = [None for _ in range(size)]
+    sc = [Signal(bool(0)) for _ in range(size+1)]
+    # faList[0] = fullAdder(x[0],y[0],Signal(bool(0)), soma[0], sc[0])
+
+    for i in range(len(faList)):
+        faList[i] = fullAdder(x[i],y[i],sc[i], soma[i], sc[i+1])
+
     @always_comb
     def comb():
-        pass
+        carry .next = sc[-1] # Last carry
 
     return instances()
